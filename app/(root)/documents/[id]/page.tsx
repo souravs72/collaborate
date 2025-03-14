ff02db2b76f6a7451c0fce2ef44b40d1bb5c9d1c
Souravs72 <souravsingh2609@gmail.com>
2025-03-15 03:17:48 +0530

    package upgrade

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,7 +1,7 @@
-import CollaborativeRoom from "@/components/CollaborativeRoom"
+import CollaborativeRoom from "@/components/CollaborativeRoom";
 import { getDocument } from "@/lib/actions/room.actions";
 import { getClerkUsers } from "@/lib/actions/user.actions";
-import { currentUser } from "@clerk/nextjs/server"
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const Document = async ({ params: { id } }: SearchParamProps) => {
@@ -29,7 +29,7 @@ const Document = async ({ params: { id } }: SearchParamProps) => {
 
   return (
     <main className="flex w-full flex-col items-center">
-      <CollaborativeRoom 
+      <CollaborativeRoom
         roomId={id}
         roomMetadata={room.metadata}
         users={usersData}
